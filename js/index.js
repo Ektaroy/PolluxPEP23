b8a90e01296c709e909b57382e516b6b59e1c30d
Ektaroy <97738792+Ektaroy@users.noreply.github.com>
2023-01-15 15:37:45 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// main home body starts here
+var options = {
+    valueNames: [ 'name', 'title', 'expertise' ]
+  };
+  
+  var userList = new List('users', options);
+//  main home body starts here
+
